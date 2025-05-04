0062d2ab8fc6903e0f7921299aca0dc4dabf2b95
Peter Wu <peter.wu@techworkslabs.com>
2025-05-04 15:43:48 +0800

    Updated Http Interface and implmentation

--- src/Modules/Crawler/Domain/Contracts/HttpClientInterface.php
+++ src/Modules/Crawler/Domain/Contracts/HttpClientInterface.php
@@ -4,10 +4,18 @@ namespace Modules\Crawler\Domain\Contracts;
 
 /**
  * Interface HttpClientInterface
- *
  * @package Modules\Crawler\Domain\Contracts
+ *
+ * This interface defines the contract for an HTTP client that can be used to make GET requests.
  */
 interface HttpClientInterface
 {
+    /**
+     * Make a GET request to the specified URL.
+     *
+     * @param string $url The URL to fetch.
+     * @return string The response body.
+     * @throws \RuntimeException If the request fails.
+     */
     public function get(string $url): string;
 }
--- src/Modules/Crawler/Infrastructure/Http/LaravelHttpClient.php
+++ src/Modules/Crawler/Infrastructure/Http/LaravelHttpClient.php
@@ -2,25 +2,33 @@
 
 namespace Modules\Crawler\Infrastructure\Http;
 
-use Modules\Crawler\Domain\Contracts\HttpClientInterface;
 use Illuminate\Support\Facades\Http;
+use Modules\Crawler\Domain\Contracts\HttpClientInterface;
 
 /**
  * Class LaravelHttpClient
- *
  * @package Modules\Crawler\Infrastructure\Http
+ *
+ * This class is responsible for making HTTP requests using Laravel's HTTP client.
  */
 class LaravelHttpClient implements HttpClientInterface
 {
     /**
-     * Send a GET request to the specified URL and return the response body.
+     * Make a GET request to the specified URL.
      *
-     * @param string $url
-     * @return string
+     * @param string $url The URL to fetch.
+     * @param array $headers Optional headers to include in the request.
+     * @return string The response body.
+     * @throws \RuntimeException If the request fails.
      */
-    public function get(string $url): string
+    public function get(string $url, array $headers = []): string
     {
+
         $response = Http::get($url);
+
+        if (! $response->successful()) {
+            throw new \RuntimeException('Failed to fetch the URL: ' . $url);
+        }
 
         return $response->body();
     }
